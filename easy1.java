e0bf2cb7542ec7222e2d412f0e49d56c16dd719e
Sanjana Rohra <119722413+sanjanarohra@users.noreply.github.com>
2023-12-05 16:06:16 +0530

    Add files via upload

---
+++ easy1.java
@@ -0,0 +1,20 @@
+import java.io.*;
+public class easy1
+{
+    public static void main(String[]args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader (System.in));
+        String s = br.readLine();
+        s = s.trim();
+        int i = s.length()-1;
+        while(i>=0 && !Character.isAlphabetic(s.charAt(i)))
+            i--;
+        int count=0;
+        while(i>=0 && Character.isAlphabetic(s.charAt(i)))
+        {
+            i--;
+            count++;
+        }
+        System.out.println(count);
+    }   
+}
