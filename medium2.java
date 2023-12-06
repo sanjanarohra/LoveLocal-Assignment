44aa0f63dcbbac8971ca12d1447e68af849d0434
Sanjana Rohra <119722413+sanjanarohra@users.noreply.github.com>
2023-12-06 13:43:41 +0530

    Add files via upload

---
+++ medium2.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class medium2 
+{
+    public static List<Integer> majorityElements(int[] arr) 
+    {
+        List<Integer> result = new ArrayList<>();
+
+        int max_ele1 = 0, count1 = 0;
+        int max_ele2 = 0, count2 = 0;
+
+        //to find two elements of highest frequency
+        for (int i : arr) {
+            if (i == max_ele1) count1++;
+            else if (i == max_ele2) count2++;
+            else if (count1 == 0) 
+            {
+                max_ele1 = i;
+                count1 = 1;
+            } 
+            else if (count2 == 0) 
+            {
+                max_ele2 = i;
+                count2 = 1;
+            } 
+            else 
+            {
+                count1--;
+                count2--;
+            }
+        }
+
+        // Count occurrences of the candidates
+        count1 = 0;
+        count2 = 0;
+        for (int i : arr) 
+        {
+            if (i == max_ele1) count1++;
+            else if (i == max_ele2) count2++;
+        }
+
+        // Check if candidates appear more than ⌊ n/3 ⌋ times
+        int n = arr.length;
+        if (count1 > n / 3) result.add(max_ele1);
+        if (count2 > n / 3) result.add(max_ele2);
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2};
+        List<Integer> result = majorityElements(arr);
+        System.out.println("Elements appearing more than ⌊ n/3 ⌋ times: " + result);
+    }
+}
